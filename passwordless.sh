9db930924e5a3bc91bff0b1e571a64f0f9e5926e
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-09-27 18:56:20 -0400

    Update passwordless.sh

--- passwordless.sh
+++ passwordless.sh
@@ -83,8 +83,8 @@ cat $PUBKEY >> $SSHDIR/authorized_keys
 chmod 600 $SSHDIR/authorized_keys
 
 
-CUSTOM_USER=lngo
-CUSTOM_SSHDIR=/users/lngo/.ssh
+CUSTOM_USER=phantomsjn
+CUSTOM_SSHDIR=/users/phantomsjn/.ssh
 CUSTOM_GROUP=`id -gn ${CUSTOM_USER}`
 
 cp $PRIVKEY ${CUSTOM_SSHDIR}
