adb0653ab7c1fbcf5bc125aa2ddb14184727429f
ryanbui00 <43189020+ryanbui00@users.noreply.github.com>
2018-09-27 19:08:49 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -9,6 +9,6 @@ sudo make
 sudo make all install
 echo "export PATH='$PATH:/opt/openmpi/3.1.2/bin'" >> /users/phantomsjn/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/'" >> /users/QD899836/.bashrc
-cd ...
+cd ..
 sudo rm -Rf openmpi-3.1.2
 sudo rm -Rf openmpi-3.1.2.tar.gz
