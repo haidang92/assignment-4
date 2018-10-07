314c0684e425aa91b264aa1d2b0845cce097aad9
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-06 20:58:36 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -60,7 +60,7 @@ for i in range(6):
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
   
-  node.addService(pg.Execute(shell="sh", command="sudo su QD899836 -c 'cp /local/repository/source/* /users/QD899836'"))
+  node.addService(pg.Execute(shell="sh", command="sudo su QD899836 -c 'cp /local/repository/source/* /users/DT882578'"))
   
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
